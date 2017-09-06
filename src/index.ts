b91d1725f24808d89eefaa1750e624c6b3f1f146
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-06 11:37:13 +0200

    handling package.json

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import exec from './exec';
 import { join } from 'path';
-import { install as npmInstall } from './npm';
+import { readFileSync } from 'fs-extra';
+import { installByName as npmInstallByName, installByJson as npmInstallByJson } from './npm';
 
 const CWD = process.cwd();
 const JOB_FOLDER = 'job';
-const [ , , cid, pkg ] = process.argv;
+const packageNamePattern = /.+/; // TODO: determine pattern
+const [ , , cid, pkgOrJson ] = process.argv;
 
 process.on('unhandledRejection', error => {
   throw error;
 });
 
-const run = async (cid: string, pkg: string) => {
-  if (!cid) throw new Error('Missing correlation id');
-  if (!pkg) throw new Error('Missing package name');
-  await exec(`rm -rf ${join(CWD, JOB_FOLDER)}`);
-  await npmInstall(pkg, JOB_FOLDER);
+const resolvePackage = (pkgOrJson: string) => {
+  try {
+    return { json: JSON.parse(pkgOrJson), pkg: null };
+  } catch (err) {
+    return packageNamePattern.test(pkgOrJson) ? { pkg: pkgOrJson, json: null } : { pkg: null, json: null };
+  }
 };
 
-run(cid, pkg);
+const run = async (cid: string, pkgOrJson: string) => {
+  if (!cid) throw new Error('Missing correlation id');
+  const { pkg, json } = resolvePackage(pkgOrJson);
+  if (!pkg && !json) throw new Error('Missing package name or package.json');
+  await exec(`rm -rf ${join(CWD, JOB_FOLDER)}`);
+  if (pkg) await npmInstallByName(pkg, JOB_FOLDER);
+  else if (json) await npmInstallByJson(json, JOB_FOLDER);
+};
+
+run(cid, pkgOrJson);
--- src/npm.ts
+++ src/npm.ts
@@ -1,4 +1,12 @@
+import { ensureDir, writeJSON } from 'fs-extra';
+import { join } from 'path';
 import exec, { ExecOptions } from './exec';
 
-export const install = (pkg: string, folder: string, options?: ExecOptions) =>
+export const installByName = (pkg: string, folder: string, options?: ExecOptions) =>
   exec(`npm install --no-save --legacy-bundling --prefix ${folder} ${pkg}`, options);
+
+export const installByJson = async (json: string, folder: string, options?: ExecOptions) => {
+  await ensureDir(folder);
+  await writeJSON(join(folder, 'package.json'), json);
+  return await exec(`npm install --legacy-bundling`, { ...options, cwd: folder });
+};
