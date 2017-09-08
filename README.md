6d7e175471cecd1d2e60de0042c3ee61ed527a17
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-08 19:07:22 +0200

    add push.sh and update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # corp-check-worker
 Worker layer for corp-check
 
-```
-aws ecr get-login --region eu-central-1
-```
+## Build image and push to private docker repository
+
+Just run `push.sh`
---
+++ push.sh
@@ -0,0 +1,5 @@
+DOCKER_LOGIN = aws ecr get-login --no-include-email --region eu-central-1
+$DOCKER_LOGIN
+docker build -t corp-check .
+docker tag corp-check:latest 856324650258.dkr.ecr.eu-central-1.amazonaws.com/corp-check:latest
+docker push 856324650258.dkr.ecr.eu-central-1.amazonaws.com/corp-check:latest
