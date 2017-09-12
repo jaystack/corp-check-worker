acddfbf30925ea8c3f04f23c416a417ac4346c39
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-12 10:23:21 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-DOCKER_LOGIN = aws ecr get-login --no-include-email --region eu-central-1
+#!/bin/bash
+DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region eu-central-1)
 $DOCKER_LOGIN
 docker build -t corp-check .
 docker tag corp-check:latest 856324650258.dkr.ecr.eu-central-1.amazonaws.com/corp-check:latest
