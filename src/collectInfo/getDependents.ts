850a1b92291e9f639283391bb1f3e99f662ddc81
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-21 13:24:58 +0200

    stabilization of getDependents

--- src/collectInfo/getDependents.ts
+++ src/collectInfo/getDependents.ts
@@ -4,17 +4,22 @@ import sleep from '../sleep';
 export default async (packageList: string[]): Promise<number[]> => {
   const dependents = [];
   for (const name of packageList) {
-    const {
-      rows: [ { value } ]
-    } = await request.get(`https://replicate.npmjs.com/registry/_design/app/_view/dependedUpon`, {
-      json: true,
-      qs: {
-        group_level: 1,
-        start_key: `["${name}"]`,
-        end_key: `["${name}", {}]`
-      }
-    });
-    dependents.push(value);
+    try {
+      const {
+        rows: [ { value } ]
+      } = await request.get(`https://replicate.npmjs.com/registry/_design/app/_view/dependedUpon`, {
+        json: true,
+        qs: {
+          group_level: 1,
+          start_key: `["${name}"]`,
+          end_key: `["${name}", {}]`
+        }
+      });
+      dependents.push(value);
+    } catch (error) {
+      console.error(error);
+      dependents.push(null);
+    }
   }
   return dependents;
 };
