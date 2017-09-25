ec71d984500ff606f58afee5609e5356c94b5e70
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-09-25 12:44:09 +0200

    use runSeries at getDependents and getGithubData

--- src/collectInfo/getDependents.ts
+++ src/collectInfo/getDependents.ts
@@ -1,25 +1,25 @@
 import request = require('request-promise-native');
 import sleep from '../sleep';
+import runSeries from '../runSeries';
 
-export default async (packageList: string[]): Promise<number[]> => {
-  const dependents = [];
-  for (const name of packageList) {
-    try {
-      const {
-        rows: [ { value } ]
-      } = await request.get(`https://replicate.npmjs.com/registry/_design/app/_view/dependedUpon`, {
-        json: true,
-        qs: {
-          group_level: 1,
-          start_key: `["${name}"]`,
-          end_key: `["${name}", {}]`
-        }
-      });
-      dependents.push(value);
-    } catch (error) {
-      console.error(error);
-      dependents.push(null);
-    }
+const getDependentCount = async (name: string): Promise<number> => {
+  try {
+    const {
+      rows: [ { value } ]
+    } = await request.get(`https://replicate.npmjs.com/registry/_design/app/_view/dependedUpon`, {
+      json: true,
+      qs: {
+        group_level: 1,
+        start_key: `["${name}"]`,
+        end_key: `["${name}", {}]`
+      }
+    });
+    return value;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-  return dependents;
 };
+
+export default (packageList: string[]): Promise<number[]> =>
+  runSeries(packageList.map(name => getDependentCount.bind(null, name)));
--- src/collectInfo/getGithubData.ts
+++ src/collectInfo/getGithubData.ts
@@ -1,6 +1,7 @@
 import request = require('request-promise-native');
 import { GithubData } from '../types';
 import getGithubIssueStats = require('gh-issues-stats');
+import runSeries from '../runSeries';
 
 const TOKEN = '5907a5849829f615a5004fbb362351f3a7897511';
 
@@ -11,22 +12,20 @@ const fetch = (owner: string, repo: string, endpoint: string = '', query?: Objec
     qs: query
   });
 
-export default async (repositories: { owner: string; repo: string }[]): Promise<GithubData[]> => {
-  const data: GithubData[] = [];
-  for (const repository of repositories) {
-    if (!repository) {
-      data.push({} as GithubData);
-      continue;
-    }
-    const { owner, repo } = repository;
-    const { stargazers_count, subscribers_count, forks_count } = await fetch(owner, repo);
-    const issueStats = await getGithubIssueStats(`${owner}/${repo}`, { tokens: [ TOKEN ] });
-    data.push({
-      starsCount: stargazers_count,
-      forksCount: forks_count,
-      subscribersCount: subscribers_count,
-      ...issueStats
-    } as GithubData);
+const getGithubData = async (repository: { owner: string; repo: string }): Promise<GithubData> => {
+  if (!repository) {
+    return {} as GithubData;
   }
-  return data;
+  const { owner, repo } = repository;
+  const { stargazers_count, subscribers_count, forks_count } = await fetch(owner, repo);
+  const issueStats = await getGithubIssueStats(`${owner}/${repo}`, { tokens: [ TOKEN ] });
+  return {
+    starsCount: stargazers_count,
+    forksCount: forks_count,
+    subscribersCount: subscribers_count,
+    ...issueStats
+  } as GithubData;
 };
+
+export default async (repositories: { owner: string; repo: string }[]): Promise<GithubData[]> =>
+  runSeries(repositories.map(repository => getGithubData.bind(null, repository)));
