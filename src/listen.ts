02c371fc8c0fe9a909681c4a42e2b1561a6b5966
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-18 11:58:38 +0200

    common handling close

--- src/listen.ts
+++ src/listen.ts
@@ -3,22 +3,22 @@ import { QUEUE_NAME, RABBIT_ENDPOINT, EXIT_TIMEOUT } from './consts';
 import run from './run';
 import sleep from './utils/sleep';
 
+const handleClose = title => async error => {
+  console.error(title, error || '');
+  await sleep(EXIT_TIMEOUT);
+  process.exit(1);
+};
+
 export default async () => {
   try {
     const connection = await amqp.connect(RABBIT_ENDPOINT);
-    connection.on('close', () => process.exit(1));
-    connection.on('error', error => {
-      console.error('CONNECTION', error);
-      process.exit(1);
-    });
+    connection.on('close', handleClose('CONNECTION CLOSE'));
+    connection.on('error', handleClose('CONNECTION ERROR'));
     const channel = await connection.createChannel();
     channel.assertQueue(QUEUE_NAME);
     channel.prefetch(1);
-    channel.on('close', () => process.exit(1));
-    channel.on('error', error => {
-      console.error('CHANNEL ERROR', error);
-      process.exit(1);
-    });
+    channel.on('close', handleClose('CHANNEL CLOSE'));
+    channel.on('error', handleClose('CHANNEL ERROR'));
     console.log('WAITING TASKS ON QUEUE:', RABBIT_ENDPOINT, QUEUE_NAME);
     channel.consume(QUEUE_NAME, async msg => {
       const { cid, pkg, packageLock, yarnLock, production } = JSON.parse(msg.content.toString());
@@ -26,8 +26,6 @@ export default async () => {
       channel.ack(msg);
     });
   } catch (error) {
-    console.error('RABBIT CONNECTION ERROR', error);
-    await sleep(EXIT_TIMEOUT);
-    process.exit(1);
+    await handleClose('COULD NOT CONNECT TO RABBIT')(error);
   }
 };
