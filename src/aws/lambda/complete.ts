064935226b5973d6d0d9078b455ef6ffcb3cfebb
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-20 14:48:19 +0200

    use http to complete and cache

--- src/aws/lambda/complete.ts
+++
@@ -1,5 +0,0 @@
-import { Result } from 'corp-check-core';
-import { COMPLETE_LAMBDA_NAME } from '../../consts';
-import invokeLambda from '../invokeLambda';
-
-export default (cid: string, data: Result) => invokeLambda(COMPLETE_LAMBDA_NAME, { cid, ...data });
--- src/aws/lambda/getCache.ts
+++
@@ -1,5 +0,0 @@
-import { Meta } from 'corp-check-core';
-import { CACHE_LAMBDA_NAME } from '../../consts';
-import invokeLambda from '../invokeLambda';
-
-export default (modules: string[]): Promise<Meta> => invokeLambda(CACHE_LAMBDA_NAME, { modules });
