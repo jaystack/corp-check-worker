0bec3bb5681832fb61573f206cb803940a505d7a
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-30 14:31:18 +0100

    add npm endpoint consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+// NOTE: to change npm registry use NPM_CONFIG_REGISTRY=http://localhost env var
+
 export const API_URL = process.env.API_URL;
 export const RABBIT_ENDPOINT = process.env.RABBIT_ENDPOINT || 'amqp://localhost';
 export const QUEUE_NAME = process.env.QUEUE_NAME || 'tasks-dev';
@@ -10,6 +12,8 @@ export const CACHE_ENDPOINT = process.env.CACHE_ENDPOINT || 'getmodulemeta';
 export const PROGRESS_LAMBDA_NAME = process.env.PROGRESS_LAMBDA_NAME || 'Progress-dev';
 export const PROGRESS_ENDPOINT = process.env.PROGRESS_ENDPOINT || 'progress';
 export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME || 'corp-check-rest-filestorage-dev';
+export const NPM_COUCHDB_ENDPOINT = process.env.NPM_COUCHDB_ENDPOINT || 'https://replicate.npmjs.com';
+export const NPM_SEARCH_ENDPOINT = process.env.NPM_SEARCH_ENDPOINT || 'https://registry.npmjs.org/-/v1/search';
 
 export const CWD = process.cwd();
 export const JOB_FOLDER = 'job';
--- src/side-effects/npm.ts
+++ src/side-effects/npm.ts
@@ -1,17 +1,21 @@
 import { stringify } from 'querystring';
+import { join } from 'path';
 import request = require('request-promise-native');
+import { NPM_COUCHDB_ENDPOINT, NPM_SEARCH_ENDPOINT } from '../consts';
 
 export const getPackages = (packageNames: string[]): any[] =>
-  request
-    .post('https://replicate.npmjs.com/registry/_all_docs', {
-      json: true,
-      body: { keys: packageNames }
-    })
-    .catch(err => ({ rows: [] }))
-    .then(({ rows }) => rows);
+  packageNames.length === 0
+    ? []
+    : request
+        .post(`${NPM_COUCHDB_ENDPOINT}/registry/_all_docs`, {
+          json: true,
+          body: { keys: packageNames }
+        })
+        .catch(err => ({ rows: [] }))
+        .then(({ rows }) => rows);
 
 export const search = (text: string, size: number): any[] =>
   request
-    .get(`https://registry.npmjs.org/-/v1/search?${stringify({ text, size })}`, { json: true })
+    .get(`${NPM_SEARCH_ENDPOINT}?${stringify({ text, size })}`, { json: true })
     .catch(err => ({ objects: [] }))
     .then(({ objects }) => objects);
