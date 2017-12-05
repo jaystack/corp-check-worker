7ff36c4cb01fa43b508b47cc2400cef4f71d689d
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-12-05 15:14:16 +0100

    handle non-string licenseTypes

--- src/side-effects/resolveLicense.ts
+++ src/side-effects/resolveLicense.ts
@@ -1,8 +1,14 @@
 import getLicenseInfo = require('get-license-npm');
 
+const stringifyLicenseType = (license: any): string => {
+  if (!license) return null;
+  if (typeof license === 'string') return license;
+  return JSON.stringify(license);
+};
+
 export default async (folder: string) => {
   const license = await getLicenseInfo(folder);
   const licenseValue = license.license || null;
   const licenseType = (licenseValue && licenseValue.type) || licenseValue;
-  return { type: licenseType, hasLicenseFile: !!license.licenseFile, isPrivate: !!license.private };
+  return { type: stringifyLicenseType(licenseType), hasLicenseFile: !!license.licenseFile, isPrivate: !!license.private };
 };
